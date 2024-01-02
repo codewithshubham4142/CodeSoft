6641361428f61e7daf14ed7541461fa5e99a06f3
codewithshubham4142 <153393272+codewithshubham4142@users.noreply.github.com>
2024-01-02 21:55:32 +0530

    index.js

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,1 +1,38 @@
+let displayValue = '0';
+
+function updateDisplay() {
+    document.getElementById('display').innerText = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '0';
+    updateDisplay();
+}
+
+function appendToDisplay(value) {
+    if (displayValue === '0') {
+        displayValue = value;
+    } else {
+        displayValue += value;
+    }
+    updateDisplay();
+}
+
+function operate(operator) {
+    displayValue += operator;
+    updateDisplay();
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue).toString();
+        updateDisplay();
+    } catch (error) {
+        displayValue = 'Error';
+        updateDisplay();
+    }
+}
+
+// Initial display update
+updateDisplay();
 
